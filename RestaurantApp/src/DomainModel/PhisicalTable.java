a03984c0f0ba0377013025ac41bc79c2849659e1
FrancescoBettazzi <54951539+FrancescoBettazzi@users.noreply.github.com>
2021-01-08 12:23:59 +0100

    Add files via upload

---
+++ RestaurantApp/src/DomainModel/PhisicalTable.java
@@ -0,0 +1,27 @@
+package DomainModel;
+
+public class PhisicalTable {
+	private int tableID;
+	private boolean available = true;
+
+	public PhisicalTable(int tableID) {
+		this.tableID = tableID;
+	}
+
+	public int getTableID() {
+		return tableID;
+	}
+
+	public void setTableID(int tableID) {
+		this.tableID = tableID;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+}
