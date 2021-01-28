230507184fe1523a4f4c4ddfaceebe73574b979c
Francesco <francesco.fantechi3@stud.unifi.it>
2021-01-28 12:19:34 +0100

    start creation PageController

---
+++ RestaurantApp/src/BusinessLogic/RestaurantManagerPageController.java
@@ -0,0 +1,12 @@
+package BusinessLogic;
+
+// TODO: Singleton
+
+public class RestaurantManagerPageController {
+	private String id=null;
+	
+	public void login(String id){
+		this.id=id;
+	}
+
+}
---
+++ RestaurantApp/src/BusinessLogic/WaiterPageController.java
@@ -0,0 +1,5 @@
+package BusinessLogic;
+
+public class WaiterPageController {
+
+}
